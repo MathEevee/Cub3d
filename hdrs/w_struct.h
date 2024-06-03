8f511e9c572b0e0facd12e58ed79d280ee1c47fd
Benoit Darenne <bedarenn@3F3.42angouleme.fr>
2024-06-03 14:23:59 +0200

    add img && struct

--- hdrs/w_struct.h
+++ hdrs/w_struct.h
@@ -6,14 +6,39 @@
 /*   By: bedarenn <bedarenn@student.42angouleme.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 13:24:01 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/06/03 13:25:15 by bedarenn         ###   ########.fr       */
+/*   Updated: 2024/06/03 13:59:46 by bedarenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef W_STRUCT_H
 # define W_STRUCT_H
 
+
 # include "w_struct_parsing.h"
 # include "w_struct_display.h"
+
+typedef struct s_coord
+{
+	int	x;
+	int	y;
+}	t_coord;
+
+typedef struct s_player
+{
+	t_coord	pos;
+	double	angle;
+}	t_player;
+
+typedef struct s_info
+{
+	char		**map;
+	t_player	base;
+	t_wimg		img_no;
+	t_wimg		img_so;
+	t_wimg		img_we;
+	t_wimg		img_ea;
+	t_color		color_f;
+	t_color		color_c;
+}	t_info;
 
 #endif // W_STRUCT_H
--- hdrs/w_typedef.h
+++ hdrs/w_typedef.h
@@ -6,13 +6,18 @@
 /*   By: bedarenn <bedarenn@student.42angouleme.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 13:27:44 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/06/03 13:28:20 by bedarenn         ###   ########.fr       */
+/*   Updated: 2024/06/03 13:58:26 by bedarenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef W_TYPEDEF_H
 # define W_TYPEDEF_H
 
-typedef int	t_fd;
+# include <stdint.h>
+
+typedef int			t_fd;
+typedef __uint32_t	t_color;
+typedef __int64_t	t_color_def;
+typedef void *		t_wimg;
 
 #endif // W_TYPEDEF_H
