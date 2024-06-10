8b913fbdee628abe038fdae3aac1380c1a60ffb7
Mathilde De Oliveira <matde-ol@2A6.42angouleme.fr>
2024-06-10 14:53:07 +0200

    glisse avec les wall

--- srcs/wmlx/wmlx_loop.c
+++ srcs/wmlx/wmlx_loop.c
@@ -6,7 +6,7 @@
 /*   By: matde-ol <matde-ol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 15:07:21 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/06/10 13:54:21 by matde-ol         ###   ########.fr       */
+/*   Updated: 2024/06/10 14:49:25 by matde-ol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ void	move_toward(t_player *player, float mv, char **map)
 
 	tmp_x = player->pos.x + (mv * cos(player->angle));
 	tmp_y = player->pos.y + (mv * sin(player->angle));
-	if (map[(int)tmp_y][(int)tmp_x] == '1')
-		return ;
-	player->pos.x = tmp_x;
-	player->pos.y = tmp_y;
+	if (map[(int)player->pos.y][(int)tmp_x] != '1')
+		player->pos.x = tmp_x;
+	if (map[(int)tmp_y][(int)player->pos.x] != '1')
+		player->pos.y = tmp_y;
 }
 
 bool	wmlx_key_update(t_joe_mama *var)
