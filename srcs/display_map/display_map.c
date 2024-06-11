0065a181b2028fb013a5f06848b54b111fe348a4
Mathilde De Oliveira <matde-ol@2A6.42angouleme.fr>
2024-06-11 17:33:38 +0200

    test map

--- srcs/display_map/display_map.c
+++ srcs/display_map/display_map.c
@@ -6,25 +6,59 @@
 /*   By: matde-ol <matde-ol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:12:03 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/06/10 13:47:51 by matde-ol         ###   ########.fr       */
+/*   Updated: 2024/06/11 16:51:34 by matde-ol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+// void	display_map(t_data *data, t_coord base, t_info info)
+// {
+// 	t_coord	pos;
+// 	char	**strs;
+// 	char	*str;
+
+// 	pos.y = base.y + SQRT_SIZE / 2;
+// 	strs = info.map;
+// 	while (*strs)
+// 	{
+// 		str = *strs;
+// 		pos.x = base.x + SQRT_SIZE / 2;
+// 		while (*str && *str != '\n')
+// 		{
+// 			if (*str == '0')
+// 				wmlx_put_square(data, pos, SQRT_SIZE, info.color_f);
+// 			else if (*str == '1')
+// 				wmlx_put_square(data, pos, SQRT_SIZE, info.color_c);
+// 			str++;
+// 			pos.x += SQRT_SIZE;
+// 		}
+// 		strs++;
+// 		pos.y += SQRT_SIZE;
+// 	}
+// }
+
 void	display_map(t_data *data, t_coord base, t_info info)
 {
 	t_coord	pos;
+	t_coord	nbr_sqrt;
+	t_coord	count;
 	char	**strs;
 	char	*str;
 
+	nbr_sqrt.y = base.x - data->max.y / SQRT_SIZE;
+	nbr_sqrt.x = base.y - data->max.x / SQRT_SIZE;
 	pos.y = base.y + SQRT_SIZE / 2;
 	strs = info.map;
-	while (*strs)
+	count.y = 0;
+	printf("nbr_sqrt.y = %d\n", nbr_sqrt.y);
+	printf("nbr_sqrt.x = %d\n", nbr_sqrt.x);
+	while (count.y != nbr_sqrt.y)
 	{
 		str = *strs;
 		pos.x = base.x + SQRT_SIZE / 2;
-		while (*str && *str != '\n')
+		count.x = 0;
+		while (count.x != nbr_sqrt.x)
 		{
 			if (*str == '0')
 				wmlx_put_square(data, pos, SQRT_SIZE, info.color_f);
@@ -32,10 +66,15 @@ void	display_map(t_data *data, t_coord base, t_info info)
 				wmlx_put_square(data, pos, SQRT_SIZE, info.color_c);
 			str++;
 			pos.x += SQRT_SIZE;
+			count.x++;
+			// printf("x = %d\n", count.x);
 		}
 		strs++;
+		count.y++;
 		pos.y += SQRT_SIZE;
+	printf("y = %d\n", count.y);
 	}
+	printf("y = %d\n", count.y);
 }
 
 void	display_minimap(t_data *data, t_info info)
