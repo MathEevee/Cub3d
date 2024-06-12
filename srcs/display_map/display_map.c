a6f3469e134ce05baf1631c2ecaabe51dbe9785f
Benoit Darenne <bedarenn@3F3.42angouleme.fr>
2024-06-12 15:53:50 +0200

    fix seg-fault

--- srcs/display_map/display_map.c
+++ srcs/display_map/display_map.c
@@ -6,7 +6,7 @@
 /*   By: bedarenn <bedarenn@student.42angouleme.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:12:03 by bedarenn          #+#    #+#             */
-/*   Updated: 2024/06/12 15:23:40 by bedarenn         ###   ########.fr       */
+/*   Updated: 2024/06/12 15:51:39 by bedarenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,16 @@ void	display_map(t_data *data, t_coord base, t_coord_part part, t_info info)
 	t_tab_str_incr	i;
 
 	pos.y = base.y + SQRT_SIZE / 2 + part.min.y * SQRT_SIZE;
-	i.strs = info.map + part.min.y;
+	i.strs = info.map;
+	while (*i.strs && i.strs < info.map + part.min.y)
+		i.strs++;
 	while (*i.strs && i.strs < info.map + part.max.y)
 	{
 		pos.x = base.x + SQRT_SIZE / 2 + part.min.x * SQRT_SIZE;
-		i.str = *i.strs + part.min.x;
-		while (*i.str && *i.str != '\n' && i.str < *i.strs + part.max.x)
+		i.str = *i.strs;
+		while (*i.str && i.str < *i.strs + part.min.x)
+			i.str++;
+		while (*i.str && i.str < *i.strs + part.max.x)
 		{
 			if (*i.str == '0')
 				wmlx_put_square(data, pos, SQRT_SIZE, info.color_f);
@@ -44,6 +48,10 @@ t_coord_part	map_coord_part(t_coord img_max, t_coord_f player)
 
 	max = set_coord(img_max.x / SQRT_SIZE + 4, img_max.y / SQRT_SIZE + 3);
 	part.min = set_coord(player.x - max.x / 2, player.y - max.y / 2);
+	if (part.min.x < 0)
+		part.min.x = 0;
+	if (part.min.y < 0)
+		part.min.y = 0;
 	part.max = set_coord(player.x + max.x / 2, player.y + max.y / 2);
 	return (part);
 }
