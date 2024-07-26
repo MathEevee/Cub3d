078c3b15b2b60dec9d936b56cbcbecaa6e3c0cd6
Benoit Darenne <bedarenn@3F3.42angouleme.fr>
2024-07-26 13:30:47 +0200

    add map coord

---
+++ srcs_bonus/utils/map_coord.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_coord.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matde-ol <matde-ol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 10:19:51 by matde-ol          #+#    #+#             */
+/*   Updated: 2024/07/26 12:32:43 by matde-ol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+#include "cub3d.h"
+
+t_coord	map_coord(t_map map)
+{
+	t_coord	size;
+
+	size.x = 0;
+	size.y = 0;
+	while (map.map[size.y])
+	{
+		size.x = 0;
+		while (map.map[size.y][size.x])
+			size.x++;
+		size.y++;
+	}
+	return (size);
+}
+
+char	map_getter(t_map map, t_coord coord)
+{
+	if (coord.x < 0 || coord.y < 0
+		|| coord.x >= map.size.x || coord.y >= map.size.y)
+		return ('\0');
+	return (map.map[coord.y][coord.x]);
+}
